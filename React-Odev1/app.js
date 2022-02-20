ead2d9b377e37931cc00ab866098e6b9fae60766
İlyas <92398458+ilyasisiner@users.noreply.github.com>
2022-02-20 15:09:19 +0300

    Add files via upload

---
+++ React-Odev1/app.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const getData = async (user_id) => {
+  if (user_id < 0 || !Number.isInteger(user_id)) {
+    return;
+  }
+
+  const { data: user } = await axios(
+    `https://jsonplaceholder.typicode.com/users/${user_id}`
+  );
+  const { data: userPosts } = await axios(
+    `https://jsonplaceholder.typicode.com/posts?userId=${user_id}`
+  );
+
+  return { ...user, posts: userPosts };
+};
+
+export default getData;
